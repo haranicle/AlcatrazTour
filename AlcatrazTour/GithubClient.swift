13e1f1401050817295cb54c335c9703c5df99be5
Kazushi Hara <hara.kazushi@po.ntts.co.jp>
2015-02-24 15:44:13 +0900

    add AlamoFire,SwiftyJSON,Realm

---
+++ AlcatrazTour/GithubClient.swift
@@ -0,0 +1,15 @@
+//
+//  GithubClient.swift
+//  AlcatrazTour
+//
+//  Copyright (c) 2015年 haranicle. All rights reserved.
+//
+
+import UIKit
+
+class GithubClient: NSObject {
+    
+    
+    
+   
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+pod 'Alamofire', '~> 1.1'
+pod 'SwiftyJSON', '~> 2.1.3'
+pod 'Realm', '~> 0.90.6'
