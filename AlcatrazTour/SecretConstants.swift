a7771ef4438773b536f31a74826dd40948f36a58
Kazushi Hara <hara.kazushi@po.ntts.co.jp>
2015-02-26 13:55:46 +0900

    ignore SecretConstants

---
+++ AlcatrazTour/SecretConstants.swift
@@ -0,0 +1,13 @@
+//
+//  SecretConstants.swift
+//  AlcatrazTour
+//
+//  Created by MTER on 2015/02/26.
+//  Copyright (c) 2015年 haranicle. All rights reserved.
+//
+
+import Cocoa
+
+class SecretConstants: NSObject {
+
+}
