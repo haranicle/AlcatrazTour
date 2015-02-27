f72e2badd764639b6a920299e31ba8993ef43d6c
Kazushi Hara <hara.kazushi@po.ntts.co.jp>
2015-02-27 12:56:11 +0900

    enable show list

--- AlcatrazTour/ViewController.swift
+++ AlcatrazTour/ViewController.swift
@@ -6,10 +6,19 @@
 //
 
 import UIKit
+import Realm
+
+enum Modes:Int {
+    case Stars = 0
+    case Update = 1
+    case New = 2
+    case Popularity = 3
+}
 
 class ViewController: UIViewController {
     
     var githubClient = GithubClient()
+    var currentMode = Modes.Stars
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +32,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - Realm
+    
+    var starsResults = Plugin.allObjects().sortedResultsUsingProperty("starGazersCount", ascending: false)
+    var updateResults = Plugin.allObjects().sortedResultsUsingProperty("updatedAt", ascending: false)
+    var newResults = Plugin.allObjects().sortedResultsUsingProperty("createdAt", ascending: false)
+    
+    func currentResult()->RLMResults {
+        switch currentMode {
+        case Modes.Stars: return starsResults
+        case Modes.Update: return updateResults
+        case Modes.New: return newResults
+        default: return starsResults
+        }
+    }
+    
     // MARK: - UI Parts
     
     @IBOutlet weak var tableView: UITableView!
@@ -30,11 +54,36 @@ class ViewController: UIViewController {
     // MARK: - Action
     
     @IBAction func onSegmentChanged(sender: UISegmentedControl) {
+        currentMode = Modes(rawValue: sender.selectedSegmentIndex)!
+        tableView.reloadData()
+        tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0), atScrollPosition: UITableViewScrollPosition.Top, animated: true)
     }
     
     @IBAction func onRefreshPushed(sender: AnyObject) {
+        githubClient.reloadAllPlugins()
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return Int(currentResult().count)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let plugin = currentResult()[UInt(indexPath.row)] as Plugin
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as PluginTableViewCell
+        cell.plugin = plugin
+        cell.titleLabel.text = plugin.name
+        cell.noteLabel.text = plugin.note
+        
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "yy/MM/dd"
+        
+        cell.statusLabel.text = "⭐️\(plugin.starGazersCount) 🔄\(formatter.stringFromDate(plugin.updatedAt)) 👺\(formatter.stringFromDate(plugin.createdAt))"
+        
+        return cell
+    }
 
 }
 
