d3c380e7a47978e87fbe56ec8192661f0b79851f
Kazushi Hara <hara.kazushi@po.ntts.co.jp>
2015-03-04 17:27:03 +0900

    Show ranking with cool cicle in cells.

--- AlcatrazTour/PluginTableViewCell.swift
+++ AlcatrazTour/PluginTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 class PluginTableViewCell: UITableViewCell {
     
     @IBOutlet weak var avaterImageView: UIImageView!
+    @IBOutlet weak var rankingLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var noteLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
